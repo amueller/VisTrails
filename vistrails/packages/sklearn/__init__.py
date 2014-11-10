62912675237e5dfb4a1cbd7043c5a9c57debd30f
Remi Rampin <remirampin@gmail.com>
2014-11-10 17:31:25 -0500

    Removes package_dependencies()

--- vistrails/packages/sklearn/__init__.py
+++ vistrails/packages/sklearn/__init__.py
@@ -3,15 +3,6 @@ name = 'sklearn'
 version = '0.15.2'
 
 
-def package_dependencies():
-    import vistrails.core.packagemanager
-    manager = vistrails.core.packagemanager.get_package_manager()
-    dependencies = []
-    if manager.has_package('org.vistrails.vistrails.spreadsheet'):
-        dependencies.append('org.vistrails.vistrails.spreadsheet')
-    return dependencies
-
-
 def package_requirements():
     import vistrails.core.requirements
     if not vistrails.core.requirements.python_module_exists('sklearn'):
