b42b884e0d7c6c57cadeacc8585aef8aa91699bf
Remi Rampin <remirampin@gmail.com>
2014-11-10 17:31:07 -0500

    Use require_python_module()

--- vistrails/packages/sklearn/__init__.py
+++ vistrails/packages/sklearn/__init__.py
@@ -4,10 +4,8 @@ version = '0.15.2'
 
 
 def package_requirements():
-    import vistrails.core.requirements
-    if not vistrails.core.requirements.python_module_exists('sklearn'):
-        raise vistrails.core.requirements.MissingRequirement('sklearn')
-    if not vistrails.core.requirements.python_module_exists('numpy'):
-        raise vistrails.core.requirements.MissingRequirement('numpy')
-    if not vistrails.core.requirements.python_module_exists('scipy'):
-        raise vistrails.core.requirements.MissingRequirement('scipy')
+    from vistrails.core.requirements import require_python_module
+
+    require_python_module('sklearn')
+    require_python_module('numpy')
+    require_python_module('scipy')
