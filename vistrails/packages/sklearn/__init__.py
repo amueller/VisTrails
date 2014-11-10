d6d9f0e777aa7e4a5c532836bfa4eafe564187a9
Remi Rampin <remirampin@gmail.com>
2014-11-10 17:34:59 -0500

    Adds sources for installbundle

--- vistrails/packages/sklearn/__init__.py
+++ vistrails/packages/sklearn/__init__.py
@@ -6,6 +6,15 @@ version = '0.15.2'
 def package_requirements():
     from vistrails.core.requirements import require_python_module
 
-    require_python_module('sklearn')
-    require_python_module('numpy')
-    require_python_module('scipy')
+    require_python_module('sklearn', {
+        'pip': 'scikit-learn',
+        'linux-debian': 'python-sklearn',
+        'linux-ubuntu': 'python-sklearn'})
+    require_python_module('scipy', {
+        'pip': 'scipy',
+        'linux-debian': 'python-scipy',
+        'linux-ubuntu': 'python-scipy'})
+    require_python_module('numpy', {
+        'pip': 'numpy',
+        'linux-debian': 'python-numpy',
+        'linux-ubuntu': 'python-numpy'})
